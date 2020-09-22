f409c04f55741c94d0a2b96740fd609fd0276622
Syalcindag <40965702+Syalcindag@users.noreply.github.com>
2020-09-22 09:47:25 +0300

    addItem() , clickItem() and deleteItem() function added in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemModel } from 'src/models/itemModel';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Todo-App';
+  item: ItemModel = {itemName: '', completed:false}
+  itemList: ItemModel[] = [];
+
+  addItem(){
+    this.itemList.push({completed: false, itemName: this.item.itemName})
+  }
+
+  clickItem(item: ItemModel){
+    let itemIndex = this.itemList.indexOf(item);
+
+    this.itemList[itemIndex].completed = true;
+  }
+
+  deleteItem(item: ItemModel){
+    let itemIndex =this.itemList.indexOf(item);
+    this.itemList.splice(itemIndex, 1);
+  }
 }
